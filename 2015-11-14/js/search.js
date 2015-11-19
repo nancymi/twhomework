edbafd1f15c0a29b606f838a6399b52c72440694
nancymi <13121178nancy@gmail.com>
2015-11-19 22:46:29 +0800

    JavaScript: modify js file while searching.

---
+++ 2015-11-14/js/search.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+	var bookmarks;
+
+	startConfiguration();
+	startInitiation();
+	runContentFilter();
+
+	function startConfiguration() {
+		$.ajaxSettings.async = false;
+		$.getJSON("https://raw.githubusercontent.com/nancymi/twhomework/gh-pages/2015-11-14/sources/bookmarks.json", function(data) {
+			bookmarks = data;
+		});
+	}
+
+	function startInitiation() {
+		var contentStr = _.chain(bookmarks).map(function(bookmark) {
+			return getBookmarkStr(bookmark.title, bookmark.created);
+		}).value();
+		$(".content").html(contentStr);
+	}
+
+	function runContentFilter() {
+		$("#keyword").bind("input propertychange", function() {
+			var keyword = $(this).val();
+			if (keyword != "") {
+				var keywordRE = new RegExp(keyword, "ig");
+
+				$(".content").html("");
+
+				var bookmarksOnFilter = bookmarks.filter(function(bookmark) {
+					return keywordRE.test(bookmark.title);
+				});
+
+				var bookmarkOnFilterStr = bookmarksOnFilter.map(function(bookmarkOnFilter) {
+					var highlightBookmarkOnFilter = bookmarkOnFilter.title.replace(
+						keywordRE, '<span style="background-color:#F24A98">'+ keyword +'</span>');
+					return getBookmarkStr(highlightBookmarkOnFilter, bookmarkOnFilter.created);
+				});
+
+				$(".content").html(bookmarkOnFilterStr);
+			} else {
+				startInitiation();
+			}			
+		});
+	}
+
+	function getLocalTime(timestamp) {
+		var now = new Date();
+		now.setTime(timestamp);
+		return now.getFullYear() + "-" + (now.getMonth()+1) + "-" + now.getDate();
+	}
+
+	function getBookmarkStr(title, created) {
+		return '<div class="bookmark">' +
+		        '<p class="title">' + title + '</p>' +
+		        '<p class="created">Created @ ' + getLocalTime(created) +'</p>' +
+		        '<div class="line"></div>'
+		        '</div>';
+	}
+});
